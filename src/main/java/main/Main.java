1dc48f5bf716427746f1165c8eba24744f5003d1
Félicien Clarté <clartefelicien@hotmail.com>
2016-05-31 11:33:37 +0200

    faut quand meme qqch dans le projet ...

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+
+import commons.MyStringUtils;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(MyStringUtils.isNullOrEmpty("qsdfsdgf"));
+	}
+}
